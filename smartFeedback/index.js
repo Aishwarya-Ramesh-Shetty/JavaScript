c77c09a6fdeddecd975dd7c0aaf5ee6496cd4220
Aishwarya-Ramesh-Shetty <shettyaishu050@gmail.com>
2026-03-07 15:22:10 +0530

    Learned about basic js concept

---
+++ smartFeedback/index.js
@@ -0,0 +1,22 @@
+let score = 0;
+const displayScore = document.getElementById('displayScore');
+const feedback = document.getElementById('displayFeedback');
+const addScore = document.getElementById('addScore');
+
+addScore.onclick = function(){
+    console.log("Button clicked");
+    score = score + 1;
+    displayScore.textContent = score;
+    if(score >=10 && score < 20){
+        feedback.textContent = "Great job! Keep up the good work!";
+    }
+    else if(score >=20){
+        feedback.textContent = "Excellent! You're doing amazing!";
+    }
+}
+
+resetScore.onclick = function(){
+    score = 0;
+    displayScore.textContent = score;
+    feedback.textContent = "Score reset. Start again!";
+}
